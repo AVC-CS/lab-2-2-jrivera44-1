ead1cf1271ace3c914e62714ba346ec387ee4d09
jrivera44 <149451158+jrivera44@users.noreply.github.com>
2024-02-20 19:03:09 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
    ##################################################
    # Code your program here
    ##################################################
-    # overtime = workhours - reg_hours
+    # overtime = work_hours - 40
     # overtime_wage = overtime * ov_rate
     # regular_wage = reg_hours * reg_rate
     # total_wage = regular_wage + overtime_wage
